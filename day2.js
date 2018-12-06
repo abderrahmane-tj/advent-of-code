e18ca10c83b4189229169fcbde102070f6532e6d
Abderrahmane TAHRI JOUTI <tj.abderrahmane@gmail.com>
2018-12-06 16:43:09 +0100

    init with day 1 & 2

---
+++ day2.js
@@ -0,0 +1,315 @@
+const strings = `jplenqtlagxhivmwmscfukzodp
+jbrehqtlagxhivmeyscfuvzodp
+jbreaqtlagxzivmwysofukzodp
+jxrgnqtlagxhivmwyscfukwodp
+jbrenqtwagjhivmwysxfukzodp
+jbrenqplagxhivmwyscfuazoip
+jbrenqtlagxhivzwyscfldzodp
+jbrefqtlagxhizmwyfcfukzodp
+jbrenqtlagxhevmwfsafukzodp
+jbrunqtlagxrivmsyscfukzodp
+jbrenqtlaguhivmwyhlfukzodp
+jbrcnstsagxhivmwyscfukzodp
+jbrenqtlagozivmwyscbukzodp
+jbrenqwlagxhivswysrfukzodp
+jbrenstlagxhuvmiyscfukzodp
+jbranqtlhgxhivmwysnfukzodp
+jbrenqtvagxhinmxyscfukzodp
+jbrenqtlagdhivmwyscfukxody
+jbrenqtlagelavmwyscfukzodp
+jbrenqtlagxhtvmwyhcfukzbdp
+jbrenqwlagxhivmwyscfutzopp
+jbrenqtlavxhibmtyscfukzodp
+jbronqtlagxnivmwyscfuzzodp
+jbredqtlagxhppmwyscfukzodp
+jbrcnqtlogxhivmwysxfukzodp
+jbremqtlagehivswyscfukzodp
+jbrenqolagxhivmcyscfukzokp
+jbrehqtlacxhgvmwyscfukzodp
+fbrlnqtlagxhivmwyscbukzodp
+zbrfnqtlagxhivrwyscfukzodp
+jbregqtlagxnivmwyscfhkzodp
+jbrenqtllgxnivmwystfukzodp
+jurenqtlagxhivmwyscfulzoup
+jbrenitdagxhivmwyxcfukzodp
+jbrenqtlagxqivmwyscvuszodp
+jbqenqwlagxhivmwyscfckzodp
+jbrenqtlagxhivmwxscqupzodp
+jbrenqtlagxhivmwysciuqiodp
+jbrjnjtlagxhivmwyscfukzode
+jbrenqtlagxhuvmwqscfukzods
+jbrenqtlagxhuvmuyscfukzudp
+ibrenqtlagxhivmwyscfuktokp
+jbsenqtlagxhivcwyscfuksodp
+jbrfnntlagxhivmwnscfukzodp
+jzrenqulagxhivmwyscfukzodx
+jbrenqtqygxhivmwyscfukzwdp
+jbrenqtlagxfixmwyscfukzcdp
+jbrenqaoagxhivmwyshfukzodp
+jbrenqtlazxhivmworcfukzodp
+jbrenqtlagxhicowyscfukrodp
+jbrqnqtlagxhivzwyzcfukzodp
+jbrenqtlalxhuvxwyscfukzodp
+jbrenqtlqgxhhviwyscfukzodp
+jbrenqtuggxhivmoyscfukzodp
+jbrenqtlagxpivmwyscfukkodw
+zbrenqhlagxhivmwyscdukzodp
+jbrenutlagxxivmwyscfukzoqp
+obrenqtlagxhivmwxscfuszodp
+jbrenqtlagxhlvmwyscfuixodp
+rbrenqtlagdhixmwyscfukzodp
+jbrenqtlagxhivmwescfyszodp
+jbrfnqtlagxhivmwyscaukzhdp
+jbrenqtiagxhivmbyscfuxzodp
+cbrrnqtuagxhivmwyscfukzodp
+jbkenqtlagxhigmwysufukzodp
+jbjewqtlagxhivmwyscfuqzodp
+jbrznqtlagxvivmwyscfukzovp
+jbrenttlacxhivmwyscfhkzodp
+jblenqtlagxhivmwylcfukaodp
+jbrenqtlagxhivmqysiftkzodp
+jbrenqtlagwhikmwyscfukqodp
+jbrenqtlegxhivmwvsckukzodp
+jbrenqwzagxhiamwyscfukzodp
+jbrenqtlagxhivcwyscfgkzodc
+jbrenqtlagxxhvmwxscfukzodp
+jbrenqtlngxhivmwyscfukoowp
+jbreomtlagxhivmwpscfukzodp
+jfrenqtlagxhivmwyscnumzodp
+jbrenqtlagphipmwyscfukfodp
+jvrenqtlagxhivmwyscfmkzodw
+jbrenqtlaxxoiemwyscfukzodp
+jbrenqtlagxhivmwyscemkzpdp
+jbrenyjldgxhivmwyscfukzodp
+jbrenqtlagxhivfvyspfukzodp
+kbrenctlakxhivmwyscfukzodp
+jbrmhqtlagxhivmwyscfuizodp
+jbjenqtlagxlivmbyscfukzodp
+jbrenqtlaaxhivmmyshfukzodp
+jbronqtlagxhirmvyscfukzodp
+jbcrnqtlagxwivmwyscfukzodp
+jxrenszlagxhivmwyscfukzodp
+jbpenqtlagxhivmwyscfukkody
+jbrewqtlawxhivmwyscfukzhdp
+jbrenqylagxhivmwlxcfukzodp
+jbrenqtlagxxivtwywcfukzodp
+jbrenqtlagxhcvgayscfukzodp
+jbrenitlagxhivmwiscfukzohp
+jbrenutlagxhivmwyscbukvodp
+nbrenqtlagxhivmwysnfujzodp
+jbrenqtlagxhivmwyqcfupzoop
+jbrenqtrarxhijmwyscfukzodp
+jbrenqtlagxhivmwysdfukzovy
+jbrrnqtlagxhivmwyvcfukzocp
+jbrenqtlagxhivmwyscfuvzzhp
+jbhenitlagxhivmwysufukzodp
+jbrenqtlagxhivmwyscfuwzoqx
+kbrenqtlagxhivmwysqfdkzodp
+jbrenqtlagxhivmwyxmfukzodx
+jbcenatlagxxivmwyscfukzodp
+jbrenhtlagvhdvmwyscfukzodp
+jxrenqtlafxhivfwyscfukzodp
+jbreaztlpgxhivmwyscfukzodp
+tqrenqtlagxfivmwyscfukzodp
+jbrenqgllgxhwvmwyscfukzodp
+jbrejjtlagxhivmgyscfukzodp
+jbrenqtlagxhivmwyscvukzoqv
+jbrvnqtlagxsibmwyscfukzodp
+jbrenqttagxhuvmwyscfukvodp
+jbrenqteagxhivmwcscfukqodp
+jbrenqtsabxhivmwyspfukzodp
+jbbenqtlagxhivmwyscjukztdp
+jnrenqtlagxhiimwydcfukzodp
+jbrenqtlagxhfvmwyscxukzodu
+jbrenqtluyxhiomwyscfukzodp
+jbrenqvlagxhivmwyscuukzolp
+ebrenqtlagxnivmwysrfukzodp
+jbreeqtlatxhigmwyscfukzodp
+jbrenqtvxgxhivmwyscfukzedp
+jbrmnqtlagxhivmwywcfuklodp
+jbreeqtvagxhivmwyscfukzody
+jbrenptlagxhivmxyscfumzodp
+jbrenqtlagxhivmwysgfukzfsp
+jurenqtlagjhivmwkscfukzodp
+jbrenntlagxhivmwtscffkzodp
+jbrenqglagxhivmwyocfokzodp
+wbrenqtlagxhivmwhscfukiodp
+jbrenqtligxhivmqascfukzodp
+jbrenqtlagxhivmwxscfukpojp
+jurenqtlagxhivmmyscfbkzodp
+jbrenqtmagxhivmwyscfrbzodp
+jcrenqtlagxhivmwysefukzodm
+jbrenqtlagxhicmwywcfukzodl
+jbvanqtlagfhivmwyscfukzodp
+jbmenqjlagxhivmwyscfdkzodp
+jbrenqtlagohivvwysctukzodp
+jbrenqtdagxdivmwyscfckzodp
+kbrefqtlagxhivmwyscfuazodp
+jbrwnqtoagxhivmwyswfukzodp
+jjhenqtlagxhivmwyscfukzorp
+jbgsnqtlagxhivkwyscfukzodp
+jbrynqtlagxhivmsyspfukzodp
+jbrenftlmkxhivmwyscfukzodp
+nbrenqtxagxhmvmwyscfukzodp
+jbrunqtlagxhijmwysmfukzodp
+jbrenqtlagmaivmwyscfukzowp
+jbrerqtlagxhihmwyscfukzudp
+jbrenqtlagahivmwysckukzokp
+kbrenqtlagxhirmwyscfupzodp
+jbrrnqtlagxhivmwyecfukzodz
+jbrenqtlavxhivmwyscfusiodp
+jnrenqtlagxhivmwyhcfukzodw
+jbretqtlagfhivmwyscfukzrdp
+jbreoqtnagxhivmwyscfukzopp
+jbrenbtllgxhivmwsscfukzodp
+jbrenqtlmgxhivmwyscfuwzedp
+jbnenqtlagxhivbwyscfukzokp
+jbrenqslagxhivmvyscfukzndp
+jbrenqtlagxzivmwuscfukztdp
+gbrenqtlagxhyvmwyscfukjodp
+jbrenqteagxhivmwyscfuszedp
+jbrenqtlaglhivmwysafukkodp
+jbrenqtlagxhcvmwascfukzogp
+jbrenqtlagxhsvmkcscfukzodp
+jbrenqslbgxhivmwyscfufzodp
+cbrenqtlagxhivkwtscfukzodp
+jbrenqtltgxhivmzyscfukzodj
+jbrgnqtlagihivmwyycfukzodp
+vbrenqauagxhivmwyscfukzodp
+jbrqnqtlagjhivmwyscfqkzodp
+jbrenqtlakxhivmwyscfukvobp
+jcrenqelagxhivmwtscfukzodp
+jbrrnqtlagxhlvmwyscfukzodw
+jbrenqtlagxhivmwkscaumzodp
+jbrenqdlagxhivmiescfukzodp
+jhrenqtlagxhqvmwyscmukzodp
+jbrenqtlaghhivmwyscfukkoyp
+jowenqtlagxyivmwyscfukzodp
+jbrenitaagxhivmwyscfqkzodp
+jbrenqtlagxhivmwyscfnkbudp
+jbyenqtlagxhivmiyscfukzhdp
+jbrenitlagxhibjwyscfukzodp
+jbrenqtlavxhjvmwpscfukzodp
+jbrenqyaagxhivmwyscflkzodp
+jbrenqylagxhivmwyicfupzodp
+jbrenqtlagxmevmwylcfukzodp
+lbrenqtlagxhiqmwyscfikzodp
+jbrenqtnarxhivmwyscfmkzodp
+jbrenqtlamxhivmwyscfnkzorp
+jbbenqtlavxhivmwyscjukztdp
+jbrenqtlagxhivmwyscfnliodp
+jbwenetlagxhivmwyscfukzodt
+jbrenatlagxhivmwysmfujzodp
+jbrsnstlagxhivmwyscfukgodp
+jbwvnitlagxhivmwyscfukzodp
+jbrenqtbagxhkvmwypcfukzodp
+jbrlnqtlafxhivmwyscfukdodp
+jbrenztlanxhivmwyscjukzodp
+jbrepqtlagxhivmwudcfukzodp
+jbrenqtlagxiivmwdscfskzodp
+jbrdjqtlagxhivmwyschukzodp
+jbrenqtoaguhivmwyccfukzodp
+jdrexqjlagxhivmwyscfukzodp
+jbrenqtlagxhovmwysckukaodp
+pbrfnqblagxhivmwyscfukzodp
+jbrenqtlagxrivgiyscfukzodp
+jbrelqtgagxhivmryscfukzodp
+jbrenqtlagxhicmwjscfikzodp
+jbrenqjlagxhivmwyscfmkjodp
+jbrenqtlagxpivmwzscgukzodp
+jbienqzlagxpivmwyscfukzodp
+jbrenqvlagxhivmwdscfukzodx
+owrenqtlagxhivmwyicfukzodp
+gbrenqtlaathivmwyscfukzodp
+jbgenqtlafxhivmwysqfukzodp
+jbrenqtlagxhivtwsscfukzokp
+jbrnnqylanxhivmwyscfukzodp
+ebrenqolagxhivmcyscfukzodp
+jbrenqtlarnhivgwyscfukzodp
+jbmenqtlagxhivmvyscfukzgdp
+jbrevqtlaglhivmwystfukzodp
+jbrenqplanthivmwyscfukzodp
+jbrenqtlagxhivmkyscbukzosp
+jbrenqtlagxaivmwyscfugzodo
+jbrenqplagxhnvmwyscfjkzodp
+jbrenqtlagxhivgwyscfusrodp
+cbrenqtlagxhivmwysmfukzody
+jbrenquwaexhivmwyscfukzodp
+jbredqtlagxhdvmwyscfukzoup
+jbrxnqtlagxhivmwysczukaodp
+jbrenqtlafnhivmwyscfuczodp
+jbbdkqtlagxhivmwyscfukzodp
+ubrenqtlagxhivkwyucfukzodp
+ebjenqtlagxhivmwyscfukzodj
+jbgenqtlugxxivmwyscfukzodp
+jbrenqtoagxqivmwdscfukzodp
+bbeenqtlagxhivmwyscfumzodp
+jbfeeqtlagxhivmwmscfukzodp
+jbrlnqtlagxhiimwescfukzodp
+jbrenqtlagwoivmwyscfukhodp
+jbrenqtlagnhivmwyscfuszovp`;
+
+const processed = strings
+.split("\n")
+.map(processKeepString)
+.filter(({ occurences }) => Object.keys(occurences).length);
+
+const processedIds = processed.map(({ string }) => string);
+
+const twos = processed.filter(({ occurences }) => occurences[2]).length;
+const threes = processed.filter(({ occurences }) => occurences[3]).length;
+
+console.log(findRightBoxes(processedIds));
+
+///////
+function buildCounter() {
+  return [..."abcdefghijklmnopqrstuvwxyz"].reduce((m, l) => {
+    m[l] = 0;
+    return m;
+  }, {});
+}
+
+function buildOccurences(string) {
+  const counter = buildCounter();
+  [...string].forEach(c => (counter[c] += 1));
+  return {
+    occurences: Object.keys(counter).reduce((thoseThatCount, letter) => {
+      const count = counter[letter];
+      if (count === 2 || count === 3) {
+        thoseThatCount[count] = letter;
+      }
+      return thoseThatCount;
+    }, {})
+  };
+}
+
+function processKeepString(string) {
+  return { string, ...buildOccurences(string) };
+}
+
+function findRightBoxes(ids) {
+  for (let i = 0; i < ids.length; i++) {
+    let a = ids[i];
+    for (let j = 0; j < ids.length; j++) {
+      let b = ids[j];
+      if (rightBoxes(a, b)) {
+        return { a, b };
+      }
+    }
+  }
+}
+
+function rightBoxes(a, b) {
+  let count = 0;
+  for (let i = 0; i < 26; i++) {
+    if (a[i] !== b[i]) {
+      count++;
+    }
+
+    if (count > 1) {
+      return false;
+    }
+  }
+
+  return count === 1;
+}
